fabiankachlock/one-online

commit 125c05f9ea0abca33d6afc075cc45ae1570ba1e2

feat: ordering players and displaying them in order

static/js/game.js
@@ -45,6 +45,27 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 var __values = (this && this.__values) || function(o) {
     var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
     if (m) return m.call(o);
@@ -137,7 +158,8 @@ var handleMessage = function (message) {
     }
 };
 var initGame = function (data) {
-    displayPlayers(playerId, data.players);
+    var orderedPlayers = reorderPlayers(playerId, data.players);
+    displayPlayers(playerId, orderedPlayers);
     var ownAmount = 0;
     state.players = data.players.map(function (p) {
         if (p.id === playerId) {
@@ -168,6 +190,13 @@ var initGame = function (data) {
     setDeckVisibility(state.isCurrent);
     setUnoCardVisibility(ownAmount === 1);
     changePlayerCardAmount(playerId, data.deck.length, playerId);
+};
+var reorderPlayers = function (id, players) {
+    var sortedPlayers = players.sort(function (a, b) { return a.order - b.order; });
+    var ownIndex = sortedPlayers.findIndex(function (p) { return p.id === playerId; });
+    var firstHalf = sortedPlayers.splice(0, ownIndex);
+    var secondHalf = sortedPlayers.splice(ownIndex + 1, sortedPlayers.length - firstHalf.length - 1);
+    return __spreadArray(__spreadArray([], __read(firstHalf.reverse())), __read(secondHalf.reverse()));
 };
 var handleGameUpdate = function (update) {
     var e_1, _a;

static/ts/game.ts
@@ -111,7 +111,8 @@ const handleMessage = (message: MessageEvent) => {
 };
 
 const initGame = (data: GameInitMessage) => {
-  displayPlayers(playerId, data.players);
+  const orderedPlayers = reorderPlayers(playerId, data.players);
+  displayPlayers(playerId, orderedPlayers);
   let ownAmount = 0;
   state.players = data.players.map(p => {
     if (p.id === playerId) {
@@ -145,6 +146,25 @@ const initGame = (data: GameInitMessage) => {
   setDeckVisibility(state.isCurrent);
   setUnoCardVisibility(ownAmount === 1);
   changePlayerCardAmount(playerId, data.deck.length, playerId);
+};
+
+const reorderPlayers = (
+  id: string,
+  players: (PlayerMeta & { order: number })[]
+): (PlayerMeta & { order: number })[] => {
+  // reorder players so, that they appear to go a circle (own id doesn't matter)
+  // example (playerID = 3) [3, 4, 6, 1, 2, 5] [2, 1, 6, 5, 4]
+
+  const sortedPlayers = players.sort((a, b) => a.order - b.order); // sort players by order ascending
+  const ownIndex = sortedPlayers.findIndex(p => p.id === playerId)!;
+
+  const firstHalf = sortedPlayers.splice(0, ownIndex);
+  const secondHalf = sortedPlayers.splice(
+    ownIndex + 1,
+    sortedPlayers.length - firstHalf.length - 1
+  );
+
+  return [...firstHalf.reverse(), ...secondHalf.reverse()];
 };
 
 const handleGameUpdate = (update: GameUpdateMessage) => {

dist/game/game.js
@@ -46,6 +46,7 @@ var notificationManager_1 = require("./notificationManager");
 var gameStoreRef_js_1 = require("../store/gameStoreRef.js");
 var index_js_1 = require("../logging/index.js");
 var optionDescriptions_1 = require("./optionDescriptions");
+var shuffle_js_1 = require("../lib/shuffle.js");
 var Game = /** @class */ (function () {
     function Game(name, password, host, isPublic, key, options) {
         var _this = this;
@@ -193,7 +194,7 @@ var Game = /** @class */ (function () {
             return playerIdMap;
         };
         this.constructPlayerLinks = function () {
-            var players = Array.from(_this.metaData.players);
+            var players = shuffle_js_1.shuffle(Array.from(_this.metaData.players));
             _this.metaData.playerLinks = {};
             players.forEach(function (p, index) {
                 var leftLink;
@@ -212,7 +213,8 @@ var Game = /** @class */ (function () {
                 }
                 _this.metaData.playerLinks[p] = {
                     left: leftLink,
-                    right: rightLink
+                    right: rightLink,
+                    order: index
                 };
             });
         };

src/game/game.ts
@@ -8,6 +8,7 @@ import { createAccessToken } from '../store/accessToken';
 import { Logging } from '../logging/index.js';
 import { LoggerInterface } from '../logging/interface.js';
 import { mapOptionsKeyToDescription } from './optionDescriptions';
+import { shuffle } from '../lib/shuffle.js';
 
 export type GameMeta = {
   playerCount: number;
@@ -17,6 +18,7 @@ export type GameMeta = {
     [id: string]: {
       left: string;
       right: string;
+      order: number;
     };
   };
 };
@@ -242,7 +244,7 @@ export class Game {
   };
 
   private constructPlayerLinks = () => {
-    const players = Array.from(this.metaData.players);
+    const players = shuffle(Array.from(this.metaData.players));
     this.metaData.playerLinks = {};
 
     players.forEach((p, index) => {
@@ -262,7 +264,8 @@ export class Game {
 
       this.metaData.playerLinks[p] = {
         left: leftLink,
-        right: rightLink
+        right: rightLink,
+        order: index
       };
     });
   };

dist/game/cards/deck.js
@@ -22,6 +22,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CardDeck = exports.getRandomCard = void 0;
+var shuffle_js_1 = require("../../lib/shuffle.js");
 var utils_js_1 = require("./utils.js");
 var getRandomCard = function () {
     return utils_js_1.CARD_DECK[Math.floor(Math.random() * utils_js_1.CARD_DECK.length)];
@@ -54,7 +55,7 @@ var CardDeck = /** @class */ (function () {
         configurable: true
     });
     CardDeck.prototype.refill = function () {
-        this.deck = __spreadArray(__spreadArray([], __read(utils_js_1.shuffle(utils_js_1.CARD_DECK))), __read(this.deck));
+        this.deck = __spreadArray(__spreadArray([], __read(shuffle_js_1.shuffle(utils_js_1.CARD_DECK))), __read(this.deck));
     };
     return CardDeck;
 }());

dist/game/cards/utils.js
@@ -21,7 +21,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from) {
     return to;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.shuffle = exports.CARD_DECK = exports.ALL_CARDS = exports.VALID_CARD_COLOR = exports.VALID_CARD_TYPES = exports.isColorCard = void 0;
+exports.CARD_DECK = exports.ALL_CARDS = exports.VALID_CARD_COLOR = exports.VALID_CARD_TYPES = exports.isColorCard = void 0;
 var type_js_1 = require("./type.js");
 var isColorCard = function (type) {
     return /\/\d$|skip$|draw2$|reverse$/.test(type);
@@ -65,12 +65,3 @@ exports.CARD_DECK = __spreadArray(__spreadArray(__spreadArray([], __read(exports
     }
 ]; })
     .flat()));
-var shuffle = function (array) {
-    var _a;
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        _a = __read([array[j], array[i]], 2), array[i] = _a[0], array[j] = _a[1];
-    }
-    return array;
-};
-exports.shuffle = shuffle;

dist/game/state/gameNotifications.js
@@ -52,7 +52,8 @@ var GameStateNotificationManager = /** @class */ (function () {
             var mapped = players.map(function (p) { return ({
                 id: p.id,
                 name: p.name,
-                cardAmount: state.decks[p.id].length
+                cardAmount: state.decks[p.id].length,
+                order: p.order
             }); });
             if (targets.length === 0) {
                 targets = players.map(function (p) { return p.id; });

dist/game/state/state.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __values = (this && this.__values) || function(o) {
     var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
     if (m) return m.call(o);
@@ -96,10 +107,10 @@ var GameStateManager = /** @class */ (function () {
         };
         this.start = function () {
             _this.Logger.info("[State] [Started] " + _this.gameId);
-            _this.notificationManager.notifyGameInit(_this.players, _this.state, _this.options);
+            _this.notificationManager.notifyGameInit(_this.players.map(function (p) { return (__assign(__assign({}, p), { order: _this.metaData.playerLinks[p.id].order })); }), _this.state, _this.options);
         };
         this.hotRejoin = function (playerId) {
-            _this.notificationManager.notifyGameInit(_this.players, _this.state, _this.options, [playerId]);
+            _this.notificationManager.notifyGameInit(_this.players.map(function (p) { return (__assign(__assign({}, p), { order: _this.metaData.playerLinks[p.id].order })); }), _this.state, _this.options, [playerId]);
         };
         this.clear = function () {
             _this.Logger.info("[State] [Cleared] " + _this.gameId);
@@ -222,7 +233,7 @@ var GameStateManager = /** @class */ (function () {
         this.pile = new deck_js_1.CardDeck(10, [], options.realisticDraw);
         this.state = {
             direction: 'left',
-            currentPlayer: Array.from(metaData.players)[Math.floor(Math.random() * this.metaData.playerCount)],
+            currentPlayer: Array.from(metaData.players)[0],
             topCard: this.pile.draw(),
             stack: [],
             decks: {}

commit 336a0d0441a539d74336e3899ffa86aa284bfa06

fix bug introduced by commit 125c05f

static/js/game.js
@@ -158,7 +158,7 @@ var handleMessage = function (message) {
     }
 };
 var initGame = function (data) {
-    var orderedPlayers = reorderPlayers(playerId, data.players);
+    var orderedPlayers = reorderPlayers(playerId, __spreadArray([], __read(data.players)));
     displayPlayers(playerId, orderedPlayers);
     var ownAmount = 0;
     state.players = data.players.map(function (p) {
@@ -207,9 +207,12 @@ var handleGameUpdate = function (update) {
     setDeckVisibility(state.isCurrent);
     setStateDirection(update.direction);
     for (var i = 0; i < state.players.length; i++) {
+        console.log('update for player: ', update.players[i].id);
         changePlayerCardAmount(playerId, update.players[i].amount, update.players[i].id);
         state.players[i].cardAmount = update.players[i].amount;
         if (update.players[i].id === playerId) {
+            console.log('is own player');
+            console.log('show uno:', update.players[i].amount === 1);
             setUnoCardVisibility(update.players[i].amount === 1);
         }
     }

static/ts/game.ts
@@ -111,7 +111,7 @@ const handleMessage = (message: MessageEvent) => {
 };
 
 const initGame = (data: GameInitMessage) => {
-  const orderedPlayers = reorderPlayers(playerId, data.players);
+  const orderedPlayers = reorderPlayers(playerId, [...data.players]);
   displayPlayers(playerId, orderedPlayers);
   let ownAmount = 0;
   state.players = data.players.map(p => {
@@ -177,6 +177,7 @@ const handleGameUpdate = (update: GameUpdateMessage) => {
   setStateDirection(update.direction);
 
   for (let i = 0; i < state.players.length; i++) {
+    console.log('update for player: ', update.players[i].id);
     changePlayerCardAmount(
       playerId,
       update.players[i].amount,
@@ -185,6 +186,8 @@ const handleGameUpdate = (update: GameUpdateMessage) => {
     state.players[i].cardAmount = update.players[i].amount;
 
     if (update.players[i].id === playerId) {
+      console.log('is own player');
+      console.log('show uno:', update.players[i].amount === 1);
       setUnoCardVisibility(update.players[i].amount === 1);
     }
   }